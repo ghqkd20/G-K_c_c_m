a0455cee3ac3eec94461a1a4aa296442ec4b9062
허정훈 <heojeonghun@heojeonghun-ui-MacBookPro.local>
2020-11-05 08:41:35 +0900

    last

--- src/select.js
+++ src/select.js
@@ -3,9 +3,9 @@ import React, {Component} from 'react'
 class Select extends Component{
 	render(){
 		return (
-			<select onChange={function(e){
-				debugger;
-				this.props.onChangePage(e.attributes.Value.nodeValue);
+			<select name = "test" onChange={function(e){
+				//debugger;
+				this.props.onChangePage(e.target.value);
 			}.bind(this)}>
 				<option value="eclipse" >eclipse</option>
 				<option value="dracula" selected="selected">dracula</option>
