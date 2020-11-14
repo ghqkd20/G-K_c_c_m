a4c5a32250257859741349b2975be914c4ddcc80
taehjoon <uuta1@DESKTOP-FP9V3SD.localdomain>
2020-11-14 21:09:48 +0900

    for judge referece

---
+++ app.js
@@ -0,0 +1,55 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var btoa = require("btoa");
+var atob = require("atob");
+var jd = require("./judge");
+
+
+var app = express();
+app.set('port',process.env.PORT || 5000);
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+    res.render("index");
+});
+
+app.post("/run", function (req, res) {
+    //console.log(req.body.code);
+    var code = req.body.code;//atob(req.body.code);
+    var stdin = req.body.stdin;//atob(req.body.stdin);
+    
+    // res.send(JSON.stringify({
+    //     "code" : req.query.code, "stdout" : req.query.stdin
+    // }));
+    jd.judge(code, stdin).then(function (data) {
+        var payload = JSON.stringify({
+            "stdout": data[0],
+            "stderr": data[1]
+        });
+        console.log(payload);
+        console.log("\n\n");
+        //console.log(btoa(payload));
+        console.log("\n\n");
+        res.json(payload)
+    }).catch(function (data) {
+        res.json("something went horribly wrong!");
+    })
+})
+
+app.get('/servertext',(req,res) =>{
+    res.send([
+        {
+            'id':1,
+            'name':'sibalu',
+            'job':'no jobs'
+        }
+    ])
+})
+
+app.listen(5000, function () {
+    console.log("serving at http://localhost:5000");
+});
